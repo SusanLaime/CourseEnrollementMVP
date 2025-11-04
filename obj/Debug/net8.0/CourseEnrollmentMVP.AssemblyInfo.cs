e20e155a476dbefb6f5765325814587a729df517
SusanLaime <susanlaimel1@upb.edu>
2025-11-03 22:03:48 -0400

    Feat: Original, no samAccountName

--- obj/Debug/net8.0/CourseEnrollmentMVP.AssemblyInfo.cs
+++ obj/Debug/net8.0/CourseEnrollmentMVP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseEnrollmentMVP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dec2aa6bab410999513a2693370f83b0d61c8e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd32bcf203de80e42690d499142c900f0c6f04b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseEnrollmentMVP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseEnrollmentMVP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
